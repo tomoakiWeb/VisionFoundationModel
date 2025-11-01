98344ac61e6acec9939b73922a0002adfcf10de4
tomoakiWeb <tomomiura0026@gmail.com>
2025-11-01 20:22:32 +0900

    Initial Commit

---
+++ VisionFoundationModel/VisionFoundationModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionFoundationModelApp.swift
+//  VisionFoundationModel
+//
+//  Created by 三浦知明 on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct VisionFoundationModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
